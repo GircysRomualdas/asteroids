6a4b0529d6b892d4aeafccfb6f45f3859ec048a0
romas <romas@gmail.com>
2025-05-06 14:23:40 +0300

    add grouping to game loop

--- main.py
+++ main.py
@@ -9,16 +9,24 @@ def main():
     dt = 0
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updatable.add(player)
+    drawable.add(player)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
